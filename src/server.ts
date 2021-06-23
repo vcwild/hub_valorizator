da57b0225b877ef650bf0102c932fa8281c9c1db
Victor <vcwildm@gmail.com>
2021-06-23 13:05:43 -0300

    create server exception handling system

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
+import "express-async-errors";
 
 import {router} from "./routes";
 
@@ -10,5 +11,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 app.listen(3000, () => console.log("Server is running!"))
