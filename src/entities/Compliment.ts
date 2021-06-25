f47b095d38692f104d533d7b4f9a4cc7a88f3326
Victor <vcwildm@gmail.com>
2021-06-25 00:13:28 -0300

    create compliment entity

---
+++ src/entities/Compliment.ts
@@ -0,0 +1,45 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, JoinColumn, ManyToOne } from "typeorm";
+
+import { v4 as uuid } from "uuid";
+import { Tag } from "./Tag";
+import { User } from "./User";
+
+
+@Entity("compliments")
+export class Compliment {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_sender: string;
+
+  @Column()
+  user_receiver: string;
+
+  @JoinColumn({name: "user_sender"})
+  @ManyToOne(() => User)
+  userSender: User;
+
+  @JoinColumn({name: "user_receiver"})
+  @ManyToOne(() => User)
+  userReceiver: User;
+
+  @JoinColumn({ name: "tag_id" })
+  @ManyToOne(() => Tag)
+  tag: Tag
+
+  @Column()
+  tag_id: string;
+
+  @Column()
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
